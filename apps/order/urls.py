0fa8b520dbf7a2138d4b075eaeae5cbf607db7bf
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-09-05 18:11:29 +0300

    cart lesson 23

---
+++ apps/order/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from apps.order import views
+
+
+urlpatterns = [
+    path('add/', views.add_to_cart, name='add_to_cart'),
+]
--- config/urls.py
+++ config/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('apps.blog.urls')),
     path('catalog/', include('apps.catalog.urls')),
-    path('user/', include('apps.user.urls'))
+    path('user/', include('apps.user.urls')),
+    path('order/', include('apps.order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
