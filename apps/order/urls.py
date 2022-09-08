98166d5e0157431edd115af7816e04ad5d0b67e0
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-09-08 18:15:58 +0300

    order create part 1

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from apps.order import views
 
-
 urlpatterns = [
-    path('', views.cart_list, name='cart_list'),
     path('add/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_list, name='cart_list'),
+    path('create/', views.create_order, name='create_order'),
 ]
