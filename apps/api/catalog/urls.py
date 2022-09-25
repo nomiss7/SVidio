391dba1cc56871ba5f11324534ac77a75a0c90d2
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-09-25 21:22:16 +0300

    api part 2(category) hw 22

--- apps/api/catalog/urls.py
+++ apps/api/catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.api.catalog.views import ProductListView, ProductDetailView, \
-    ProductCreateView, ProductUpdateView, ImageViewSet, ProductDeleteView
+from apps.api.catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ImageViewSet, ProductDeleteView, CategoryViewSet, CategoryCreateView, CategoryUpdateView, CategoryDeleteView, \
+    CategoryDetailView
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
@@ -9,9 +10,14 @@ urlpatterns = [
     path('product/update/<int:pk>/', ProductUpdateView.as_view()),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view()),
     path('product/<int:pk>/', ProductDetailView.as_view()),
+    path('category/create/', CategoryCreateView.as_view()),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view()),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view()),
+    path('category/<int:pk>/', CategoryDetailView.as_view()),
 ]
 
 router = DefaultRouter()
 router.register('product/image', ImageViewSet, basename='product_image')
+router.register('category', CategoryViewSet, basename='category')
 
 urlpatterns += router.urls
