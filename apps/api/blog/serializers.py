87a3b2c335efdb1b955a14865d46a5a74e08c9f3
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-10-03 13:16:27 +0300

    api part 2(blog) lesson 29

--- apps/api/blog/serializers.py
+++ apps/api/blog/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from apps.blog.models import Article
+from apps.blog.models import Article, Tag
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name',)
+
+
+class ArticleReadSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
 
     class Meta:
         model = Article
@@ -14,4 +21,21 @@ class ArticleSerializer(serializers.ModelSerializer):
             'name',
             'text_preview',
             'text',
+            'tags',
+        )
+
+
+class ArticleWriteSerializer(serializers.ModelSerializer):
+    tags = serializers.ListField(child=serializers.CharField(max_length=64), write_only=True)
+
+    class Meta:
+        model = Article
+        fields = (
+            'id',
+            'category',
+            'image',
+            'name',
+            'text_preview',
+            'text',
+            'tags',
         )
