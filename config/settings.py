6e62ea1cfaa201bae7ad46920756b519be67f989
nomiss7 <89870042+Decisionbot@users.noreply.github.com>
2022-10-23 11:02:57 +0300

    First version of application moved into github

--- config/settings.py
+++ config/settings.py
@@ -13,16 +13,21 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.contrib import staticfiles
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w41m^0kya=nmp7jltt3&_+)1k*v(tsyt$ys06oufzkh@&178#z'
+# SECRET_KEY = 'django-insecure-w41m^0kya=nmp7jltt3&_+)1k*v(tsyt$ys06oufzkh@&178#z'
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-w41m^0kya=nmp7jltt3&_+)1k*v(tsyt$ys06oufzkh@&178#z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
@@ -53,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,3 +168,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
